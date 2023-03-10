GNUWeeb/GNUWeebBot2

commit ed90ca0e78e268a57412f9ade0f0b574b2e4ef03

lib/tgapi: Use curl_easy_escape to encode special characters
This commit updates our code to use the curl_easy_escape function to
properly encode special characters in URLs. This will help ensure that
our application can handle a wider range of inputs and prevent errors
when accessing certain resources.

By using this function, we're also improving the security of our
application by preventing potential attacks that could exploit unencoded
special characters in URLs.

Reviewed-by: Alviro Iskandar Setiawan <alviro.iskandar@gnuweeb.org>
Signed-off-by: Dwiky Rizky Ananditya <kyzsuki@gnuweeb.org>

lib/tgapi.c
@@ -651,12 +651,19 @@ int tgapi_call_send_message(struct tg_api_ctx *ctx,
 	/*
 	 * TODO(ammarfaizi2): Use POST method and encode the data.
 	 */
+	
+	char *escape_text = curl_easy_escape(ch, call->text, strlen(call->text));
+	if (!escape_text)
+		return -ENOMEM;
+    
 	snprintf(url, sizeof(url),
 		 "https://api.telegram.org/bot%s/sendMessage?chat_id=%" PRId64
 		 "&text=%s&reply_to_message_id=%" PRId64,
-		 ctx->token, call->chat_id, call->text,
+		 ctx->token, call->chat_id, escape_text,
 		 call->reply_to_message_id);
 
+	curl_free(escape_text);
+	
 	printf("Curl exec to: %s\n", url);
 	curl_easy_setopt(ch, CURLOPT_URL, url);
 	curl_easy_setopt(ch, CURLOPT_WRITEFUNCTION, tgapi_curl_write);

commit efc41e10b95d55ee09ba39342d1da7aa6236ebf2

lib/tgapi: Fix `-Wshorten-64-to-32` warning
When using clang as the compiler, it yields the following warning:

  tgapi.c:655:55: warning: implicit conversion loses integer precision: \
  'unsigned long' to 'int' [-Wshorten-64-to-32]

     char *escape_text = curl_easy_escape(ch, call->text, strlen(call->text));
                         ~~~~~~~~~~~~~~~~                 ^~~~~~~~~~~~~~~~~~
  1 warning generated.

It happens because the third argument of curl_easy_escape() is an
integer, but the return type of strlen() is a size_t. Fix it by casting
the return value of strlen() to int.

Fixes: ed90ca0e78e268a57412f9ade0f0b574b2e4ef03 ("lib/tgapi: Use curl_easy_escape to encode special characters")
Signed-off-by: Alviro Iskandar Setiawan <alviro.iskandar@gnuweeb.org>

lib/tgapi.c
@@ -652,7 +652,8 @@ int tgapi_call_send_message(struct tg_api_ctx *ctx,
 	 * TODO(ammarfaizi2): Use POST method and encode the data.
 	 */
 	
-	char *escape_text = curl_easy_escape(ch, call->text, strlen(call->text));
+	char *escape_text = curl_easy_escape(ch, call->text,
+					     (int)strlen(call->text));
 	if (!escape_text)
 		return -ENOMEM;
     